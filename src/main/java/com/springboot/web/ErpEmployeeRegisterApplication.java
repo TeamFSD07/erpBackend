2e3e02b3671524fa0a1cd13b5dda42eb1b472837
Team_FSD07 <singhkk8899@gmail.com>
2024-09-13 15:35:34 +0530

    added .env file

--- src/main/java/com/springboot/web/ErpEmployeeRegisterApplication.java
+++ src/main/java/com/springboot/web/ErpEmployeeRegisterApplication.java
@@ -2,8 +2,10 @@ package com.springboot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "file:.env", ignoreResourceNotFound = true)
 public class ErpEmployeeRegisterApplication {
 
 	public static void main(String[] args) {
